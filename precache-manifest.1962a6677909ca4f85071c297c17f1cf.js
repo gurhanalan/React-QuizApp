bccc653cbf001129d967e42f0fe5af0e26ea2b62
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-06-28 22:26:51 +0300

    Updates

---
+++ precache-manifest.1962a6677909ca4f85071c297c17f1cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da19549b22c88dcb3853f13b3ffd317a",
+    "url": "/React-QuizApp/index.html"
+  },
+  {
+    "revision": "a2890619db83b7c0ba30",
+    "url": "/React-QuizApp/static/css/main.67a9dfe3.chunk.css"
+  },
+  {
+    "revision": "3cd677eb59797262ba44",
+    "url": "/React-QuizApp/static/js/2.d4621af0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-QuizApp/static/js/2.d4621af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2890619db83b7c0ba30",
+    "url": "/React-QuizApp/static/js/main.ba748d37.chunk.js"
+  },
+  {
+    "revision": "ab1f82a0be6238a1591d",
+    "url": "/React-QuizApp/static/js/runtime-main.18a7ab55.js"
+  }
+]);
